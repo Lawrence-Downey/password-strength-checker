ff84ae3038bd192c86843fabb17f6bb75a663e9c
Lawrence.Downey <cooperandalex@outlook.com>
2021-11-30 23:47:19 -0330

    FInally got the images to work.

--- jquery.password-strength-checker.js
+++ jquery.password-strength-checker.js
@@ -1,21 +1,35 @@
 (function($) {
-    $.fn.passwordStrengthChecker = function() {
+    $.fn.passwordStrengthChecker = function(options) {
         $("#passwordInput").focus()
 
+        let settings = $.extend({
+            overlay: 'rgba(0, 0, 0, 0.5)',
+            closeButton: {
+                src: null,
+                width: "40px",
+                height: "40px"
+            },
+            imageBorder: "5px solid white",
+            borderRadius: "5px",
+            imageWidth: "500px",
+            imageHeight: "400px"
+        }, options)
 
         return this.each(function() {
+            let modalOverlay;
+            let strengthImage;
+            let closeButton;
             showHidePassword();
             verify();
             clear();
-           
+            setModalOverlayProperties();
+            closeButtonProperties();
             
-            
-            
+
             function verify() {
                 $("#verify").click( (evt) => {
                     evt.preventDefault();
-                    passwordInput();
-                    strengthVerification();
+                    verification();
                 });
             };
             
@@ -43,32 +57,19 @@
                 });
             };
 
-            function checked() {
-                $("#images").click( () => {
-                    if($("#images").prop("checked") == true){
-                        alert("true")
-                       
-                    }else if($("#images").prop("checked") == false){
-                        strengthCheck();
-                    }
-                })
-            }
-
-            function passwordInput(){
+            function verification(){
                 let password = $("#passwordInput").val();
+                let strength = 0;   
                 if(password == ""){
                     $("#strength").html("");
                 }
                 else{
-                    let meter = strengthCheck(password);
+                    let meter = strengthVerification(password,strength);
                     $("#strength").html(meter);
                 }
             };
 
-            function strengthVerification(){
-                let strength = 0;
-                let password = $("#passwordInput").val();
-               
+            function strengthVerification(password,strength){      
                 if(password.match(/(.*[A-Za-z])/)){
                     strength += 1;
                 }
@@ -81,22 +82,26 @@
                 if(password.match(/(.*[~,!,@,#,$,%,^,&,*,_].*[~,!,@,#,$,%,^,&,*,_])/)){
                     strength +=1;
                 }
-                if(password.length >=7 && password.length <= 10){
+                if(password.length >=6 && password.length <= 10){
                     strength += 1;
                 }
                 if(password.length > 10){
                     strength += 1;
                 }
-                checked();
+                                
+                if($("#images").prop("checked") == true){
+                    strengthCheckImages(password, strength);   
+                }else if($("#images").prop("checked") == false){
+                    strengthCheck(password,strength);
+                }
             };
 
-            function strengthCheck(){
+            function strengthCheck(password, strength){
                 if(password.length <6){
                     $("#progress").addClass("w-25").addClass("bg-danger");
                     $("#strength").addClass("short");
                     return "I'm sorry but your password is too short. It's almost like you want to get hacked...";
-                }
-                if(strength < 3){
+                }if(strength < 3){
                     $("#progress").removeClass().addClass("progress-bar").addClass("w-50").addClass("bg-warning");
                     $("#strength").addClass("weak");
                     return "Do you work for the NL Health System? Because your password is WEAK!";
@@ -111,43 +116,91 @@
                 }
             };
             
-            function strengthCheckImages(){
+            function strengthCheckImages(password, strength){
                 if(password.length <6){
-                    $("#progress").addClass("w-25").addClass("bg-danger");
                     $("#strength").addClass("short");
+                    modalOverlay.css({opacity:0.1}).show().animate({opacity:1});
                     
-                }
-                if(strength < 3){
-                    $("#progress").removeClass().addClass("progress-bar").addClass("w-50").addClass("bg-warning");
+                }if(strength < 3){
                     $("#strength").addClass("weak");
+                    modalOverlay.css({opacity:0.1}).show().animate({opacity:1});
                     
                 }else if(strength == 3 || strength == 4){
-                    $("#progress").removeClass().addClass("progress-bar").addClass("w-75").addClass("bg-primary");
                     $("#strength").addClass("good");
+                    modalOverlay.css({opacity:0.1}).show().animate({opacity:1});
                     
                 }else{
-                    $("#progress").removeClass().addClass("progress-bar").addClass("w-100").addClass("bg-success");
                     $("#strength").addClass("strong");
+                    modalOverlay.css({opacity:0.1}).show().animate({opacity:1});
                     
                 }
             };
             
-
-            function setModalOverlay(){
-                let modalOverlay = $("<div></div>");
+            function setModalOverlayProperties(){
+                modalOverlay = $('<div></div>');
                 modalOverlay.css({
-                    "background" : "black, 0.5",
+                    "background" : settings.overlay,
                     "position" : "absolute",
                     "top" : "0px",
                     "left" : "0px",
                     "display" : "none",
                     "text-align" : "center",
-                    "width" : "50%",
-                    "height" : "50%",
-                    "padding-top" : "5%"
+                    "width" : "100%",
+                    "height" : "100%",
+                    "padding-top" : "12%",
+                    "padding-right": "10%"
                 });
+                if($("#strength").attr("class", "short")){
+                    strengthImage = $('<img src="images/TooShort.png">');
+                    imageProperties(strengthImage);
+                    modalOverlay.append(strengthImage);
+                }else if($("#strength").attr("class", "weak")){
+                    strengthImage = $('<img src="images/Weak.jpg>"');
+                    imageProperties(strengthImage);
+                    modalOverlay.append(strengthImage);
+                }else if($("#strength").attr("class", "good")){
+                    strengthImage = $('<img src="images/Good.png">');
+                    imageProperties(strengthImage);
+                    modalOverlay.append(strengthImage);
+                }else if($("#strength").attr("class", "strong")){
+                    strengthImage = $('<img src="images/Strong.jpg">');
+                    imageProperties(strengthImage);
+                    modalOverlay.append(strengthImage);
+                }    
                 $("form").append(modalOverlay);
-            }
+            };
+
+            function imageProperties(strengthImage){
+                strengthImage.css({
+                    "border":settings.imageBorder,
+                    "border-radius":settings.borderRadius,
+                    "width":settings.imageWidth,
+                    "height":settings.imageHeight
+                })
+            };
+
+            function closeButtonProperties(){
+                let symbol = {
+                    "color": "white",
+                    "cursor": "pointer",
+                    "font-size": "20px",
+                    "width": settings.closeButton.width,
+                    "height": settings.closeButton.height,
+                    "position": "absolute",
+                    "top": "5px",
+                    "right": "5px",
+                    "border": "0px",
+                    "z-index": "1"
+                }
+                
+                closeButton = $("<span>X</span>");
+                closeButton.css(symbol);
+                modalOverlay.append(closeButton);
+
+                closeButton.click(function() {
+                    modalOverlay.hide();
+                    });
+              };
         });                 
     };    
 }(jQuery));
--- script.js
+++ script.js
@@ -1,23 +1,7 @@
 "use strict"
 
-
-
-
-
 $(document).ready(() => {
     
-   
+    $("#verify").passwordStrengthChecker();
     
-    $("#verify").passwordStrengthChecker().verify();
-    passwordStrengthChecker().clear();
-
-    $("#passwordInput").passwordStrengthChecker().showHidePassword();
-    $("#passwordInput").passwordStrengthChecker().passwordInput();
-    passwordStrengthChecker().strengthCheck();
-    
-
-
-    
-      
-    
-})
+});
